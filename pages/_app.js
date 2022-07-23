fefa7208e1b647b16d2c00781e7e82be989fce6a
bacale <bacale86@gmail.com>
2022-07-23 06:55:26 +0100

    feat(GetInitialProps): understanding get initial props

--- pages/_app.js
+++ pages/_app.js
@@ -3,12 +3,23 @@ import Layout from "../components/Layout/Layout";
 import "semantic-ui-css/semantic.min.css";
 
 class MyApp extends App {
+  /***** This method enables us to use getInitialProps in all of our pages  ******/
+  static async getInitialProps({ Component, ctx }) {
+    let pageProps = {};
+
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx);
+    }
+
+    return { pageProps };
+  }
+
   render() {
-    const { Component } = this.props;
+    const { Component, pageProps } = this.props;
 
     return (
-      <Layout>
-        <Component />
+      <Layout {...pageProps}>
+        <Component {...pageProps} />
       </Layout>
     );
   }
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
+import axios from "axios";
 
-const index = () => {
+const Index = ({ posts }) => {
+  // console.log(data);
   return (
-    <div>index</div>
-  )
-}
+    <div>
+      {posts && posts.length > 0 && posts.map((el) => <div key={el.title}>{el.title}</div>)}
+    </div>
+  );
+};
 
-export default index
+Index.getInitialProps = async (ctx) => {
+  try {
+    const res = await axios.get("https://jsonplaceholder.typicode.com/posts");
+
+    const { name } = ctx.query
+
+    console.log(name)
+
+    return { posts: res.data };
+  } catch (error) {
+    return { errorLoading: true };
+  }
+};
+
+export default Index;
